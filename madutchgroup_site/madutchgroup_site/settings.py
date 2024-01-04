ffcd64e145e73aed4fc7a3b1f4fbc5cfb09b4fb4
ThabangLigavha710 <thabangdanligavha@gmail.com>
2024-01-05 00:50:39 +0200

    modified files

--- madutchgroup_site/madutchgroup_site/settings.py
+++ madutchgroup_site/madutchgroup_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s-!27hh4ttv19w%)!jx+%)-%lsrlvo(sjc1w+r(ge%(crltr+k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
