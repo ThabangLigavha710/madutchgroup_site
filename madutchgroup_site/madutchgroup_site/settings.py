1d29045e53150ff4acfb6d966be87e52f36f6d8e
ThabangLigavha710 <thabangdanligavha@gmail.com>
2024-01-05 17:01:00 +0200

    Changed database

--- madutchgroup_site/madutchgroup_site/settings.py
+++ madutchgroup_site/madutchgroup_site/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import socket
+# import psycopg2
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -82,8 +83,12 @@ WSGI_APPLICATION = 'madutchgroup_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'aBeBeF2-E6116-df3C634FAdDdg*FG4f',
+        'HOST': 'viaduct.proxy.rlwy.net',
+        'PORT': '46889',
     }
 }
 
