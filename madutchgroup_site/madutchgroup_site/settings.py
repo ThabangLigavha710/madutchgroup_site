64c2cd8983f010bec30e253a4416fb6c38220fac
ThabangLigavha710 <thabangdanligavha@gmail.com>
2024-01-05 19:32:58 +0200

    Configured URL Patterns

--- madutchgroup_site/madutchgroup_site/settings.py
+++ madutchgroup_site/madutchgroup_site/settings.py
@@ -141,10 +141,10 @@ STATICFILES_FINDERS = (
     #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
     )
 
-# MEDIA_ROOT = BASE_DIR / 'media'
-# MEDIA_URL = '/media/'
-MEDIA_URL = "/media/"
-MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
+MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+# MEDIA_URL = "/media/"
+# MEDIA_ROOT = os.environ["RAILWAY_VOLUME_MOUNT_PATH"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
